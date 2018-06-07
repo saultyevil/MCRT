a5859655e53763da09d53c92108892f0bba31128
Edward Parkinson <saultyevil@gmail.com>
2018-06-07 15:08:33 +0100

    Updated OO code to properly include scattering AND absorption :^).

--- Python/plane_oo.py
+++ Python/plane_oo.py
@@ -7,98 +7,96 @@ class Photon_Packet(object):
 
     def __init__(self):
         """
-        Initalise the photon packet.
+        Emits a new photon packet.
         """
-
         self._coords = np.array([0, 0, 0], dtype=np.float)
         self._costheta = np.sqrt(np.random.rand())
         self._sintheta = np.sqrt(1 - self._costheta ** 2)
-        self._phi = 2 * np.pi * np.random.rand()
-        self._cosphi = np.cos(self._phi)
-        self._sinphi = np.sin(self._phi)
+        self._cosphi = np.cos( 2 * np.pi * np.random.rand())
+        self._sinphi = np.sqrt(1 - self._cosphi ** 2)
         self._escaped = False
+        
+        return None
 
     def _scatter_packet(self):
         """
         Isotropic scattering of the photon packet, i.e. change the direction
         it propagates.
         """
-
         self._costheta = 2 * np.random.rand() - 1
         self._sintheta = np.sqrt(1 - self._costheta ** 2)
-        self._phi = 2 * np.pi * np.random.rand()
-        self._cosphi = np.cos(self._phi)
-        self._sinphi = np.sin(self._phi)
+        self._cosphi = np.cos(2 * np.pi * np.random.rand())
+        self._sinphi = np.sqrt(1 - self._cosphi ** 2)
+        
+        return 0
 
     def _transport_packet(self, tau_max, albedo):
         """
         Transport the photon packets through the atmosphere.
         """
-
         while self._coords[2] >= 0 and self._coords[2] <= 1:
-            # sample an optical depth and update the position of the packet
+            # sample a random optical depth and update the position of the 
+            # packet
             tau = -np.log(np.random.rand())
             L = tau/tau_max
             self._coords[0] += L * self._sintheta * self._cosphi
             self._coords[1] += L * self._sintheta * self._sinphi
             self._coords[2] += L * self._costheta
 
-            if np.random.rand() < albedo and self._coords[2] >= 0 and \
-                    self._coords[2] <= 1:
-                self._scatter_packet()
-
             # if z < 0, the photon has travelled deeper into the atmosphere
             # and is lost, hence restart the photon
             if self._coords[2] < 0:
                 self.__init__()
-
             # if z > 1, the photon has escaped the atmosphere
-            if self._coords[2] > 1:
+            elif self._coords[2] > 1:
                 self._escaped = True
+            # else it's still in the atmosphere and scatter or absorb it
+            else:
+                xi = np.random.rand()
+                if xi < albedo:
+                    self._scatter_packet()
+                else:
+                    break
+                
+        return 0
 
-
-def bin_photons(mu, mu_bins, n_photons):
+def init_photon_bins(mu_bins):
     """
-    Bins the photons into corresponding angle bins corresponding to the
-    direction they left the atmosphere.
+    Initialise the arrays used to bin photons
 
     Parameters
     ----------
-    mu: (1 x n_photons) array of floats.
-        The angle mu = cos(theta) for which the photon packets exited
-        the atmospehre.
     mu_bins: integer.
         The number of theta angles to bin for.
-    n_photons: integer.
-        The total number of photon packets.
 
     Returns
     -------
     mu_hist: (1 x mu_bins) array of ints.
-        The number of photons packets which left the atmosphere at one
-        of the binned angles.
+        An array of 0's.    
     theta: (1 x mu_bins) array of floats.
         The binned theta angles.
     """
-
     theta = np.zeros(mu_bins)
-    mu_hist = np.zeros(mu_bins)
+    mu_hist = np.zeros_like(theta)
 
-    # calculate the binned theta angles
     dtheta = 1/mu_bins
-    half_width = 0.5 * dtheta  # see kenny wood's code for half_width...?
+    half_width = 0.5 * dtheta  # see kenny wood's code for half_width reason
     for i in range(mu_bins):
-        theta[i] = np.arccos(i * dtheta + half_width) * (180/np.pi)
-
-    # bin the mu = cos(theta) angles for each photon accordingly
-    for i in range(n_photons):
-        j = abs(int(mu[i] * mu_bins))
-        mu_hist[j] = mu_hist[j] + 1
+        theta[i] = np.arccos(i * dtheta + half_width)
 
     return mu_hist, theta
 
+def bin_photon(cos_theta, mu_bins, mu_hist):
+    """
+    Bin a photon into the appropriate theta bin
+    """
+    i = abs(int(cos_theta * mu_bins))
+    mu_hist[i] += 1
+    
+    return mu_hist
 
-def photon_moments(mu_bins, mu_hist, theta):
+
+def photon_intensity(mu_bins, mu_hist, theta, n_photons):
     """
     Calculate the mean intensity for the bin angles.
 
@@ -111,19 +109,19 @@ def photon_moments(mu_bins, mu_hist, theta):
         of the binned angles.
     theta: (1 x mu_bins) array of floats.
         The binned theta angles.
+    n_photons: int.
+        The number of photons used in the simulation.
 
     Returns
     -------
-    intensity: (1 x mu_bins) array of floats.
+    intens: (1 x mu_bins) array of floats.
         The intensity estimated via the number of photon for each binned angle.
     """
-
-    intensity = np.zeros(mu_bins)
+    intens = np.zeros(mu_bins)
     for i in range(mu_bins):
-        intensity[i] = (mu_hist[i] * mu_bins)/(2 * np.sum(mu_hist) *
-                                               np.cos(theta[i] * np.pi/180))
+        intens[i] = (mu_hist[i] * mu_bins)/(2 * n_photons * np.cos(theta[i]))
 
-    return intensity
+    return intens
 
 
 # =============================================================================
@@ -132,31 +130,29 @@ def photon_moments(mu_bins, mu_hist, theta):
 
 print('Beginning Simulation...\n')
 
-n_photons = int(10e4)
-mu_bins = 10
-tau_max = 10
-albedo = 1
+# Simulation Parameters
+n_photons = int(10e6)
+mu_bins = 45
+tau_max = 3
+albedo = 0.5
+output_freq = int(10e5)
 
 start = timeit.default_timer()
-
 mu = np.zeros(n_photons)
 phi = np.zeros(n_photons)
+mu_hist, theta = init_photon_bins(mu_bins)
+
 for packet in range(n_photons):
     photon_packet = Photon_Packet()
     photon_packet._transport_packet(tau_max, albedo)
     if photon_packet._escaped is True:
-        mu[packet] = photon_packet._costheta
-        phi[packet] = photon_packet._phi
-
-    if ((packet + 1) % 10000 == 0):
+        mu_hist = bin_photon(photon_packet._costheta, mu_bins, mu_hist)
+    if ((packet + 1) % output_freq == 0):
         percent_complete = 100 * (packet + 1)/n_photons
         print('{} photons ({:3.1f}%) transported.'
               .format(packet + 1, percent_complete))
-
-mu_hist, theta = bin_photons(mu, mu_bins, n_photons)
-energy_absorbed = mu_hist/n_photons
-intensity = photon_moments(mu_bins, mu_hist, theta)
-
+        
+intensity = photon_intensity(mu_bins, mu_hist, theta, n_photons)
 stop = timeit.default_timer()
 
 print('\nTransport of {} packets completed in {:3.2f} seconds.'
@@ -171,16 +167,9 @@ fig = plt.figure(figsize=(17, 8))
 # plot the intensity against angle
 ax1 = fig.add_subplot(121)
 ax1.plot(theta, intensity, 'kx')
-ax1.set_xlabel(r'Angle, $\theta$')
+ax1.set_xlabel(r'$cos(\theta)$')
 ax1.set_ylabel(r'Normalised Intensity')
-ax1.set_xlim(0, 90)
-
-# plot the energy absorbed against angle
-ax2 = fig.add_subplot(122)
-ax2.plot(theta, energy_absorbed, 'kx')
-ax2.set_xlabel(r'Angle, $\theta$')
-ax2.set_ylabel(r'Energy Absorbed, $E_{abs}$')
-ax2.set_xlim(0, 90)
+ax1.set_xlim(0, 0.5 * np.pi)
 
 plt.savefig('mcrt_plane.pdf')
 plt.show()
