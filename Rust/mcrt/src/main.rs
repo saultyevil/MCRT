fa95316c2ef5306ae212c10ee6ff6303dda060e5
Edward Parkinson <e.j.parkinson@soton.ac.uk>
2021-05-02 21:57:01 +0300

    seemingly working photon transport

--- Rust/mcrt/src/main.rs
+++ Rust/mcrt/src/main.rs
@@ -4,7 +4,8 @@
 
 // extern crate time;
 use time::PreciseTime;
-use rand::distributions::{Distribution, Uniform};
+use rand::Rng;
+use std::f64;
 
 struct PhotonPacket{
     x: f64,
@@ -17,20 +18,15 @@ struct PhotonPacket{
     escaped: bool
 }
 
+fn random_number() -> f64 {
+    return rand::thread_rng().gen_range(0.0, 1.0);
+}
 
 fn emit_photon() -> PhotonPacket {
-
-    let between = Uniform::from(0.0..1.0);
-    let mut rng = rand::thread_rng();
-
-    let xi1: f64 = between.sample(&mut rng);
-    let xi2: f64 = between.sample(&mut rng);
-
-    let cos_theta = xi1.sqrt();
-    let sin_theta = (1.0 - cos_theta.powf(2.0)).sqrt();
-    let cos_phi = (2.0 * std::f64::consts::PI * xi2).cos();
-    let sin_phi = (1.0 - cos_phi.powf(2.0)).sqrt();
-
+    let cos_theta = f64::sqrt(random_number());
+    let sin_theta = f64::sqrt(1.0 - cos_theta.powf(2.0));
+    let cos_phi = f64::cos(2.0 * std::f64::consts::PI * random_number());
+    let sin_phi = f64::sqrt(1.0 - cos_phi.powf(2.0));
     let photon = PhotonPacket{
         x: 0.0, y: 0.0, z: 0.0, cos_theta: cos_theta, sin_theta: sin_theta,
         cos_phi: cos_phi, sin_phi: sin_phi, escaped: false
@@ -40,38 +36,38 @@ fn emit_photon() -> PhotonPacket {
 }
 
 fn scatter_photon(photon: &mut PhotonPacket) {
-
-    let between = Uniform::from(0.0..1.0);
-    let mut rng = rand::thread_rng();
-    let xi1: f64 = between.sample(&mut rng);
-    let xi2: f64 = between.sample(&mut rng);
-
-    photon.cos_theta = xi1.sqrt();
-    photon.sin_theta = (1.0 - photon.cos_theta.powf(2.0)).sqrt();
-    photon.cos_phi = (2.0 * std::f64::consts::PI * xi2).cos();
-    photon.sin_phi = (1.0 - photon.cos_phi.powf(2.0)).sqrt();
+    photon.cos_theta = f64::sqrt(random_number());
+    photon.sin_theta = f64::sqrt(1.0 - photon.cos_theta.powf(2.0));
+    photon.cos_phi = f64::cos(2.0 * std::f64::consts::PI * random_number());
+    photon.sin_phi = f64::sqrt(1.0 - photon.cos_phi.powf(2.0));
 }
 
-
+fn move_photon(photon: &mut PhotonPacket, length: f64) {
+    photon.x += length * photon.sin_theta * photon.cos_phi;
+    photon.y += length * photon.sin_theta * photon.sin_phi;
+    photon.z += length * photon.cos_theta;
+}
 
 fn transport_photons(n_photons: i32, progress: i32, tau_max: f64, scat_albedo: f64) {
-
-    let between = Uniform::from(0.0..1.0);
-    let mut rng = rand::thread_rng();
-
-
-    for n in 0..n_photons {
+    for n in 1..n_photons + 1 {
         let mut photon = emit_photon();
-        let mut tau = 0.0;
 
-        while tau < tau_max {
+        while photon.escaped == false {
+            let tau_scat = -1.0 * f64::ln(random_number());
+            let dist_move = tau_max / tau_scat;
+            move_photon(&mut photon, dist_move);
 
-            let xi = between.sample(&mut rng);
-            if xi < scat_albedo {
-                scatter_photon(&mut photon);
+            if photon.z < 0.0 {
+                photon = emit_photon();
             }
-
-            tau += 0.5;
+            else if photon.z > 1.0 {
+                photon.escaped = true;
+            }
+            else {
+                if random_number() < scat_albedo {
+                    scatter_photon(&mut photon);
+                }
+            }
         }
 
         if n % progress == 0 {
@@ -82,16 +78,16 @@ fn transport_photons(n_photons: i32, progress: i32, tau_max: f64, scat_albedo: f
 
 
 fn main() {
-
+    println!("Beginning simulation");
     let start_time = PreciseTime::now();
 
     /*
      * Intialize parameters which control the simulation
      */
 
-    let n_photons : i32 = 1e6 as i32;
+    let n_photons : i32 = 1e5 as i32;
     let progress : i32 = n_photons / 10;
-    let scat_albedo : f64 = 0.5;
+    let scat_albedo : f64 = 1.0;
     let tau_max: f64 = 7.0;
 
     transport_photons(n_photons, progress, tau_max, scat_albedo);
