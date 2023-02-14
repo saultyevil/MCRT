b88c6723420402b1bd04042e9fc5a981d97b27a7
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2023-02-14 15:14:54 +0000

    adding MPI version of plane_oo

---
+++ Python/plane_oo_mpi.py
@@ -0,0 +1,206 @@
+"""
+A simple script to transport a generation of photons through a 1D homogenous
+infinite slab of material. Output is a plot of the photon intensity against
+the escape angle from the slab.
+"""
+
+import timeit
+import numpy as np
+from matplotlib import pyplot as plt
+from mpi4py import MPI
+
+
+class PhotonPacket:
+    """Photon packet object. Contains all the functions to create and scatter
+    photons."""
+
+    def __init__(self):
+        """
+        Creates a new photon with a random isotropic direction.
+        """
+
+        self.coords = np.zeros(3, np.float32)
+        self.costheta = np.sqrt(np.random.rand())
+        self.sintheta = np.sqrt(1 - self.costheta**2)
+        self.cosphi = np.cos(2 * np.pi * np.random.rand())
+        self.sinphi = np.sqrt(1 - self.cosphi**2)
+        self.escaped = False
+
+    def isotropic_scatter(self):
+        """
+        Scatter a photon isotropically.
+        """
+
+        self.costheta = 2 * np.random.rand() - 1
+        self.sintheta = np.sqrt(1 - self.costheta**2)
+        self.cosphi = np.cos(2 * np.pi * np.random.rand())
+        self.sinphi = np.sqrt(1 - self.cosphi**2)
+
+
+class EscapedHistogram:
+    """Histogram object. Used to track the number of photons which have escaped
+    along the various escaping angles."""
+
+    def __init__(self, n_bins):
+        """
+        Create a new histogram.
+
+        Parameters
+        ----------
+        n_bins: int
+            The number of bins to make up the histogram.
+        """
+
+        self.n_bins = n_bins
+        self.weight = np.zeros(n_bins, dtype=np.float32)
+        self.theta = np.zeros(n_bins, dtype=np.float32)
+        self.intensity = np.zeros(n_bins, dtype=np.float32)
+        d_theta = 1 / n_bins
+        half_width = 0.5 * d_theta
+        for i in range(n_bins):
+            self.theta[i] = np.arccos(i * d_theta + half_width)
+
+    def bin_photon(self, cos_theta):
+        """
+        Bin a photon to the weight histogram.
+
+        Parameters
+        ----------
+        cos_theta: float
+            The escaping cos(theta) direction of the photon.
+        """
+
+        self.weight[abs(int(cos_theta * self.n_bins))] += 1
+
+    def calculate_intensity(self, n_photons):
+        """
+        Calculate the mean intensity of the binned escape angles.
+
+        Parameters
+        ----------
+        n_photons: int.
+            The number of photons in the simulation.
+
+        Returns
+        -------
+        intensity: np.ndarray
+            The intensity for each binned angle.
+        """
+
+        self.intensity = self.weight * self.n_bins / (2 * n_photons * np.cos(self.theta))
+        return self.intensity
+
+    def write_to_file(self):
+        """
+        Write the histogram to file.
+        """
+
+        with open("intensity.txt", "w", encoding="utf-8") as file_out:
+            file_out.write("angle weight intensity\n")
+            for n in range(self.n_bins):
+                file_out.write(f"{self.theta[n]} {self.weight[n]} {self.intensity[n]}\n")
+
+
+def transport_photon_packet(photon, tau_max, albedo):
+    """
+    Transport a photon packet through the atmosphere.
+
+    Parameters
+    ----------
+    photon: PhotonPacket
+        The photon to transport.
+    tau_max: float
+        The transverse optical depth of the atmosphere.
+    albedo: float
+        The scattering albedo in the atmosphere.
+    """
+
+    while photon.escaped is False:
+        # sample a random optical depth and update the position of the
+        # packet
+
+        length = -np.log(np.random.rand()) / tau_max
+        photon.coords[0] += length * photon.sintheta * photon.cosphi
+        photon.coords[1] += length * photon.sintheta * photon.sinphi
+        photon.coords[2] += length * photon.costheta
+
+        # if z < 0, the photon has traveled deeper into the atmosphere
+        # and is lost, hence restart the photon
+        # if z > 1, the photon has escaped the atmosphere
+        # else it's still in the atmosphere and scatter or absorb it
+
+        if photon.coords[2] < 0:
+            photon = PhotonPacket()
+        elif photon.coords[2] > 1:
+            photon.escaped = True
+        else:
+            if np.random.rand() < albedo:
+                photon.isotropic_scatter()
+            else:
+                break
+
+    return photon
+
+
+def main():
+    """Main function of the script"""
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    n_ranks = comm.Get_size()
+
+    n_photons = int(1e5)
+    n_photons_per_process = n_photons // n_ranks
+
+    n_bins = 45
+    tau_max = 7
+    albedo = 1.0
+    output_freq = n_photons // 10
+
+    if rank == 0:
+        print("Beginning Simulation...\n")
+        start = timeit.default_timer()
+
+    hist = EscapedHistogram(n_bins)
+    theta_bins = hist.theta
+
+    n_start = rank * n_photons_per_process
+    n_stop = (rank + 1) * n_photons_per_process
+
+    for i in range(n_start, n_stop):
+        pp = PhotonPacket()
+        pp = transport_photon_packet(pp, tau_max, albedo)
+
+        if pp.escaped:
+            hist.bin_photon(pp.costheta)
+
+        if i % output_freq == 0 and rank == 0:
+            percent_complete = 100 * i / n_photons_per_process
+            print("{:3.1f}% photons transported.".format(percent_complete))
+
+    # comm.Reduce([hist.weight, MPI.FLOAT], [hist.weight, MPI.FLOAT], op=MPI.SUM, root=0)
+    comm.Allreduce(MPI.IN_PLACE, hist.weight, op=MPI.SUM)
+
+    if rank == 0:
+        intensity = hist.calculate_intensity(n_photons)
+        hist.write_to_file()
+
+    stop = timeit.default_timer()
+
+    if rank == 0:
+        print("\nTransport of {} packets completed in {:3.2f} seconds.".format(n_photons, stop - start))
+
+        _, ax = plt.subplots(figsize=(12, 5))
+        ax.plot(theta_bins, intensity, "kx")
+        ax.set_xlabel(r"$cos(\theta)$")
+        ax.set_ylabel(r"Normalised Intensity")
+        ax.set_xlim(0, 0.5 * np.pi)
+        plt.show()
+
+
+# =============================================================================
+# Simulation
+# =============================================================================
+
+if __name__ == "__main__":
+    main()
